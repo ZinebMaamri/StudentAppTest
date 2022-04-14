0e6d370e36c125c93e0970afeabde6c49dcd1bc4
Zineb Maamri <zinebmaamri73@gmail.com>
2022-04-14 11:22:07 +0000

    second commit

--- src/test/java/com/studentapplication/springboot/StudentApplicationSpringBootApplicationTests.java
+++ src/test/java/com/studentapplication/springboot/StudentApplicationSpringBootApplicationTests.java
@@ -3,7 +3,7 @@ package com.studentapplication.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=ApplicationTests.class)
 class StudentApplicationSpringBootApplicationTests {
 
 	@Test
